13150cc797aa67bef9df9f7204b50186a74ff2c8
Lili Rodriguez <lilimrod@gmail.com>
2020-03-14 05:04:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map(source_array)
   new_array = []
   i = 0
   while i < source_array.length do
-    new_array.push(source_array[i])
+    new_array.push(yield source_array[i])
     i += 1
   end
     return new_array
