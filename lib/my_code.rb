0fe08f833144bf53c844157cf4511822903c5c8f
Lili Rodriguez <lilimrod@gmail.com>
2020-03-14 05:02:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # Your Code Here
+def map(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array.push(source_array[i])
+    i += 1
+  end
+    return new_array
+end
